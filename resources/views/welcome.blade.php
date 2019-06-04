33688e9d0027cf5dd16dfd9390259c86c196e630
นราธร ข้องหลิม <33591517+Khonglim@users.noreply.github.com>
2019-06-04 12:47:52 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,28 +4,16 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>BookSearching</title>
-  <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="expires" content="never" />
-  <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="{{ asset('asset/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
-  <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('asset/bower_components/font-awesome/css/font-awesome.min.css') }}">
-  <!-- Ionicons -->
   <link rel="stylesheet" href="{{ asset('asset/bower_components/Ionicons/css/ionicons.min.css') }}">
-  <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('asset/dist/css/AdminLTE.min.css') }}">
-  <!-- DataTables -->
   <link rel="stylesheet" href="{{ asset('asset/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') }}">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="{{ asset('asset/dist/css/skins/_all-skins.min.css') }}">
-
-
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-
-
   <style>
   #overlay {
     position: absolute;
@@ -48,18 +36,11 @@
     height: 100%;
     overflow: hidden;
 }
-
-
-
   </style>
-
-
 </head>
-<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
 <body class="hold-transition skin-purple  layout-top-nav">
         <div id="overlay"></div>
 <div class="wrapper">
-
   <header class="main-header">
     <nav class="navbar navbar-static-top">
       <div class="container">
@@ -69,8 +50,6 @@
             <i class="fa fa-bars"></i>
           </button>
         </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
           <ul class="nav navbar-nav">
           <li class="active"><a href="{{url('/')}}"> <i class=" fa fa-folder-open"></i> รายการหนังสือภาษาไทย<span class="sr-only">(current)</span></a></li>
@@ -81,32 +60,17 @@
         </div>
 
       </div>
-      <!-- /.container-fluid -->
     </nav>
   </header>
-  <!-- Full Width Column -->
   <div class="content-wrapper">
     <div class="container">
-      <!-- Content Header (Page header) -->
       <section class="content-header">
         <h1>
           ค้นหาตู้หนังสือ
 
         </h1>
-      <!--   <ol class="breadcrumb">
-          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-          <li><a href="#">Layout</a></li>
-          <li class="active">Top Navigation</li>
-        </ol> -->
       </section>
-
-      <!-- Main content -->
       <section class="content">
-
-
-
-
-
                       <form action="/search" method="POST" role="search">
                         @csrf
                         <div class="input-group">
@@ -246,7 +210,7 @@
       <div class="pull-right hidden-xs">
         <b>Version</b> 1.0.0
       </div>
-      <strong>Copyright &copy; 2019 <a href="https://adminlte.io">โปรเจกนักศึกษา</a>.</strong> KMUTNB
+      <strong>Copyright &copy; 2018 <a href="https://adminlte.io">โปรเจกนักศึกษา</a>.</strong> KMUTNB
     </div>
     <!-- /.container -->
   </footer>
